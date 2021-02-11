b12307932be63bf5afb35aec2c93a84590322272
Dell <Dell@DESKTOP-3KLGESU>
2021-02-11 10:14:51 +0530

    third commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("test of CI/CD");
 		System.out.println("second commit");
+		System.out.println("testing ci/cd !");
 	}
 
 }
